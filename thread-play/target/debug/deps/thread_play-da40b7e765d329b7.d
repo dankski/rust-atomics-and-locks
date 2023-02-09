26c4f2744868b0d6ee2014d9241167d7d0b11f8d
giacomo <giacomo@amoroso.me>
2023-02-09 19:40:48 +0100

    Play with unjoined Threads

--- thread-play/target/debug/deps/thread_play-da40b7e765d329b7.d
+++
@@ -1,5 +0,0 @@
-/Users/giacomo/github/rust-atomics-and-locks/thread-play/target/debug/deps/thread_play-da40b7e765d329b7: src/main.rs
-
-/Users/giacomo/github/rust-atomics-and-locks/thread-play/target/debug/deps/thread_play-da40b7e765d329b7.d: src/main.rs
-
-src/main.rs:
--- thread-play/target/debug/thread-play.d
+++
@@ -1,1 +0,0 @@
-/Users/giacomo/github/rust-atomics-and-locks/thread-play/target/debug/thread-play: /Users/giacomo/github/rust-atomics-and-locks/thread-play/src/main.rs
